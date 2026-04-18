0d68a67ea3376dc48bceeb7bf8c1beb8a83e25de
github user name <github@example.com>
2026-04-18 14:19:02 +0900

    feat: rebuild todo app with improved state management implement edit functionality with edditingId state add outside click detecion using useEffect and ref implement filter logic (all / active / completed) refactor structure for cleaner state handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,76 @@
-import { useState } from 'react'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import type { Todo } from './types'
 import type { filter } from './types'
-import  TodoItem  from './Todoitem'
+import  TodoItem from './Todoitem'
 import './App.css'
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([])
   const [inputText, setInputText] = useState<string>("")
-  const [searchText, setSearchText] = useState<string>("")
   const [filter, setFilter] = useState<filter>("all")
   const [editingId, setEditingId] = useState<number | null>(null)
   
 
-  const handleAddTodos = () => {
-    if (inputText.trim() === "") return
-    setTodos((prev) => [...prev, {id: Date.now(), text: inputText, status: "active", isEditing: false}])
+  const handleAddTodos = (text: string) => {
+    if (text.trim() === "") return 
+
+    const addTodo: Todo = {id: Date.now(), text: text, status: "active", isEditing: false}
+    setTodos((prev) => [...prev, addTodo])
     setInputText("")
   }
-  
+
   const handleToggle = (id: number) => {
     setTodos((prev) => prev.map(todo => (
       todo.id === id ? {...todo, status: todo.status === "active" ? "completed" : "active"} : todo
     )))
   }
+  
+  const handleEdit = (id: number, text: string) => {
+    if (text.trim() === "") return 
 
-  const handleEditTodos = (id: number, text: string) => {
-    if (text.trim() === "" ) return 
     setTodos((prev) => prev.map(todo => (
       todo.id === id ? {...todo, text: text, isEditing: false} : todo
     )))
   }
 
-
-  const filteredTodo = () => {
-    if (filter === "all") return todos
-    if (filter === "active")  return todos.filter((todo) => todo.status === "active")
-    else if (filter === "completed") return todos.filter((todo) => todo.status === "completed")
-    return todos
-  }
-
-  useEffect(() => {
-    const handleClick = () => {
-      setEditingId(null)
-    }
-
-    document.addEventListener("click", handleClick)
-
-    return () => {
-      document.removeEventListener("click", handleClick)
-    }
-  }, [])
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === "active") return todo.status === "active"
+    if (filter === "completed") return todo.status === "completed"
+    return true
+  })
+  
+  
 
   return (
     <>
-      <input
-        value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            handleAddTodos()
-          }
-        }}
-      />
-      
-      <button onClick={handleAddTodos}>追加</button>
-
+      <div>
+        <input
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleAddTodos(inputText)
+            }
+          }}/>
+          <button onClick={() => handleAddTodos(inputText)}>追加</button>
+      </div>
       <div>
         <button onClick={() => setFilter("all")}>全て</button>
-        <button onClick={() => setFilter("completed")}>達成</button>
         <button onClick={() => setFilter("active")}>未達成</button>
+        <button onClick={() => setFilter("completed")}>達成</button>
       </div>
-
-      {filteredTodo().map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          editingId={editingId}
-          setEditingId={setEditingId}
-          onToggle={handleToggle}
-          onEdit={handleEditTodos}
+      <div>
+        {filteredTodos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            editingId={editingId}
+            setEditingId={setEditingId}
+            onToggle={handleToggle}
+            onEdit={handleEdit}
           />
-      ))}
+        ))}
+      </div>
     </>
   )
 }
--- src/Todoitem.tsx
+++ src/Todoitem.tsx
@@ -1,42 +1,60 @@
 import type { Todo } from './types'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useRef } from 'react'
 
 type Props = {
     todo: Todo
     editingId: number | null
-    setEditingId: React.Dispatch<React.SetStateAction<number | null>>
+    setEditingId: (id: number | null) => void
     onToggle: (id: number) => void
     onEdit: (id: number, text: string) => void
 }
 
-function TodoItem({ todo, editingId, setEditingId, onToggle, onEdit }: Props) {
+function TodoItem({ todo, editingId, setEditingId, onToggle, onEdit}: Props) {
 
   const [editText, setEditText] = useState<string>(todo.text)
-  const inputRef = useRef<HTMLInputElement>(null)
-  const wrapperRef = useRef<HTMLDivElement>(null)
   const isEditing = editingId === todo.id
+  const editRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    editRef.current?.focus()
+  }, [isEditing])
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (editRef.current && !editRef.current.contains(e.target as Node)){
+        setEditingId(null)
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [isEditing])
 
     return (
       <>
-      <div ref={wrapperRef}>
-        <button onClick={() => onToggle(todo.id)}>{todo.status === "active" ? "□" : "☑"}</button>
-        {isEditing ? (
-          <input ref={inputRef}
-            value={editText}
-            autoFocus
-            onChange={(e) => setEditText(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                onEdit(todo.id, editText)
-              }
-            }} />
-          ) : todo.text}
+        <div>
+          <button onClick={() => onToggle(todo.id)}>{todo.status === "active" ? "□" : "☑"}</button>
+          {isEditing ? (
+            <input ref={editRef}
+              id="auto-focus"
+              value={editText}
+              onChange={(e) => setEditText(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  onEdit(todo.id, editText)
+                  setEditingId(null)
+                }
+              }}/>
+          ): todo.text}
 
-        {isEditing ? (
-          <button onClick={() => { onEdit(todo.id, editText); setEditingId(null)}}>保存</button>
-        ) : <button onClick={(e) => { e.stopPropagation(); setEditingId(todo.id) }}>編集</button> }
-      </div>
+          {isEditing ? 
+          <button onClick={(e) => {e.stopPropagation(), onEdit(todo.id, editText), setEditingId(null)}}>保存</button>
+          : <button onClick={(e) => {e.stopPropagation(), setEditingId(todo.id) }}>編集</button>}
+        </div>
       </>
     )
 }
